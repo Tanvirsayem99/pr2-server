041377c1a4cb2ef76d8b43aef2c686e902eea523
Tanvirsayem99 <107928205+Tanvirsayem99@users.noreply.github.com>
2023-05-19 22:35:20 +0600

    cnnect to vercel

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ async function run() {
       const data = await toysCollection.findOne(query);
       res.send(data)
     })
-    app.put('/update/:id', async(req, res)=>{
+    app.patch('/update/:id', async(req, res)=>{
       const id = req.params.id
       const query = {_id: new ObjectId(id)};
       const body = req.body;
